178b6a3de1653d48b4360bc3bf77be55e022a989
Piotr Szyma <thompson2908@gmail.com>
2020-01-26 17:13:08 +0100

    Version bumped

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mcl",
-    version="0.0.2",
+    version="0.0.3",
     author="Piotr Szyma",
     author_email="thompson2908@gmail.com",
     description="Python wrapper for mcl library",
